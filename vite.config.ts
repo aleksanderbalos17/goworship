b842d9f003703de4e1564fee920209970e7257bd
Developer <68669927+aleksanderbalos17@users.noreply.github.com>
2025-05-31 00:45:33 +0800

    deploy to Heroku

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        // target: 'http://localhost:8080',
+        target: "https://admin.goworship.co.uk",
         changeOrigin: true,
         secure: false,
       },
