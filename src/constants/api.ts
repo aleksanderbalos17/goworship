ea66f56ab3361795703dc62715fab1ca70782ec2
aleksanderbalos17 <68669927+aleksanderbalos17@users.noreply.github.com>
2025-06-10 13:37:37 +0800

    Updated api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,2 @@
-// Use relative path for development - this will be proxied by Vite
-export const ADMIN_BASE_URL = "/api/admin";
+// export const ADMIN_BASE_URL = 'http://localhost:8080/api/admin';
+export const ADMIN_BASE_URL = "https://admin.goworship.co.uk/api/admin";
